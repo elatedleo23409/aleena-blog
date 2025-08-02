c75ce1d099910836249336098a9936cc933bd2b1
elatedleo23409 <aryandesai95@gmail.com>
2025-08-02 23:02:16 +0530

    Initial Commit

---
+++ src/consts.ts
@@ -0,0 +1,6 @@
+// Place any global data in this file.
+// You can import this data from anywhere in your site by using the `import` keyword.
+
+export const SITE_TITLE = "Aleena's Blog";
+export const SITE_DESCRIPTION =
+  "Exploring the intersection of law, culture, and social justice. Advocating for traditional art forms and the rights of indigenous artistic communities.";
